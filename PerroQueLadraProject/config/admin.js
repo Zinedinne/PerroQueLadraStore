2374de8323b8927fc2cf801cdc175c5585f15940
Obet <obethernandez396@gmail.com>
2025-12-30 10:28:21 -0600

    feat: homepage brand with homepage and producto content types

--- PerroQueLadraProject/config/admin.js
+++ PerroQueLadraProject/config/admin.js
@@ -1,4 +1,5 @@
-module.exports = ({ env }) => ({
+
+export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
@@ -9,12 +10,5 @@ module.exports = ({ env }) => ({
     token: {
       salt: env('TRANSFER_TOKEN_SALT'),
     },
-  },
-  secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
 });
