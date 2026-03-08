c9908cad7c7d6968ee6efb566624a37f4566c740
Obet <obethernandez396@gmail.com>
2026-03-08 16:26:54 -0600

    Tienda 1.0

---
+++ PerroQueLadraProject/src/api/evento-precio/routes/evento-precio.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * evento-precio router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::evento-precio.evento-precio');
